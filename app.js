a4c518cbb8b183048ef645a6e63ce9439d3e6f83
=Nikhil <=>
2023-07-21 16:26:12 +0530

    js changes

--- app.js
+++ app.js
@@ -5,28 +5,36 @@ let form_close =  document.querySelector(".close_btn")
 let form = document.querySelector(".form__pop")
 let overlay = document.querySelector(".overlay")
 
-assessment_btn.addEventListener("click",()=>{
-    form.style.display = "block"
-    form.style.animation = "slide-in-bottom 0.5s ease both"
-    overlay.style.display = "block"
-   
-})
 
-overlay.addEventListener('click',()=>{
-    form.style.animation = "slide-out-bottom 0.5s  ease  both"
-    overlay.style.display = "none"
-    setTimeout(()=>{
-        form.style.display = "none"
-    },400)
-})
-form_close.addEventListener("click",()=>{
-    form.style.animation = "slide-out-bottom 0.5s ease both"
-    overlay.style.display = "none"
-    setTimeout(()=>{
-        form.style.display = "none"
-    },400)
-     
-})
+function toVisible(btn,el1,el2,el3="",animation){
+
+    btn.addEventListener("click",()=>{
+        el1.style.display = "block"
+        el1.style.animation = animation
+        el2.style.display = "block"
+        el3.style.display = "block"
+       
+    })
+
+}
+
+
+function toHide(arr,el1,el2,el3="",animation){
+   arr.forEach(btn=> btn.addEventListener('click',()=>{
+        el1.style.animation = animation
+        el2.style.display = "none"
+        setTimeout(()=>{
+            el1.style.display = "none"
+        },400)
+        el3.style.display = "none"
+    }))
+
+}
+
+
+toVisible(assessment_btn,form,overlay,"","slide-in-bottom 0.5s ease both")
+toHide([form_close,overlay],form,overlay,"","slide-out-bottom 0.5s  ease  both")
+
 
 
 // for mobile view
@@ -38,57 +46,18 @@ let mobile_view_btn = document.querySelector(".mobile__view-btn")
 let mobile_sidebar_ul = document.querySelector(".mobile__view-sidebar ul")
 
 
-mobile_view_btn.addEventListener("click",()=>{
-    mobile_overlay.style.display ="block"
-    mobile_sidebar.style.animation = "left-slide-in 0.2s ease-out both"
-    mobile_sidebar.style.display = "block"
-    mobile_sidebar_ul.style.display = "block"
-    
-})
 
-mobile_close.addEventListener("click",()=>{
-   
-
-    mobile_sidebar.style.animation = "left-slide-out 0.2s ease-out both"
-    mobile_overlay.style.display ="none"
-    mobile_sidebar_ul.style.display = "none"
-})
-
-mobile_overlay.addEventListener("click",()=>{
-    mobile_sidebar.style.animation = "left-slide-out 0.2s ease-out both"
-    mobile_overlay.style.display ="none"
-    mobile_sidebar_ul.style.display = "none"
-  
-})
-
-
+toVisible(mobile_view_btn,mobile_sidebar,mobile_overlay,mobile_sidebar_ul,"left-slide-in 0.2s ease-out both")
+toHide([mobile_overlay,mobile_close],mobile_sidebar,mobile_overlay,mobile_sidebar_ul,"left-slide-out 0.2s ease-out both")
 
 
 // for mobileview toggle
 
-
-let mobile_view_stats = document.querySelector(".mobile__view-stats")
-let chart_icon = document.querySelector(".mobile__stat-toggle")
-
-$(document).ready(function(){
-    $(mobile_view_stats).hide()
-
-    $(chart_icon).click(function(){
-        
-        $(mobile_view_stats).slideToggle(300)
-        $(chart_icon).toggleClass("active-stat-toggle")
-    })
-
-    
-})
-
-
-
 let desktop_view = document.querySelector(".desktop_view")
 let mobile_view  = document.querySelector(".mobile_view")
-
 let mobile_icon = document.querySelector(".mobile-icon")
 let desktop_view_btn = document.querySelector(".desktop__view-btn")
+
 
 mobile_icon.addEventListener("click",()=>{
     desktop_view.style.display = "none"
@@ -104,40 +73,37 @@ desktop_view_btn.addEventListener('click',()=>{
 
 
 
+
+
 let mobile_form = document.querySelector(".mobile_form__pop")
 let mobile_form_overlay = document.querySelector(".mobile_overlay")
 let mobile_assessment_btn = document.querySelector(".mobile__view-first-assessment")
 let mobile_view_add = document.querySelector(".mobile__view-add-btn")
 let mobile_close_btn =  document.querySelector(".mobile_form_close")
 
-mobile_assessment_btn.addEventListener("click",()=>{
-    mobile_form_overlay.style.display  = "block"
-    mobile_form.style.display = 'block'
-    mobile_form.style.animation = "slide-in-bottom 0.5s ease both"
-})
 
 
+toVisible(mobile_assessment_btn,mobile_form,mobile_form_overlay,undefined,"slide-in-bottom 0.5s ease both")
+toVisible(mobile_view_add,mobile_form,mobile_form_overlay,undefined,"slide-in-bottom 0.5s ease both")
+toHide([mobile_form_overlay,mobile_close_btn],mobile_form,mobile_form_overlay,undefined,"slide-out-bottom 0.5s ease both")
 
-mobile_view_add.addEventListener("click",()=>{
-    mobile_form_overlay.style.display  = "block"
-    mobile_form.style.display = 'block'
-    mobile_form.style.animation = "slide-in-bottom 0.5s ease both"
-})
-
-
-mobile_form_overlay.addEventListener("click",()=>{
-    mobile_form_overlay.style.display  = "none"
-    mobile_form.style.animation = "slide-out-bottom 0.5s ease both"
-    setTimeout(() => {
-        mobile_form.style.display = "none"
-    }, 510);
-})
+// 
 
 
-mobile_close_btn.addEventListener("click",()=>{
-    mobile_form_overlay.style.display  = "none"
-    mobile_form.style.animation = "slide-out-bottom 0.5s ease both"
-    setTimeout(() => {
-        mobile_form.style.display = "none"
-    }, 510);
+// for stats toggling
+
+
+let mobile_view_stats = document.querySelector(".mobile__view-stats")
+let chart_icon = document.querySelector(".mobile__stat-toggle")
+
+$(document).ready(function(){
+    $(mobile_view_stats).hide()
+
+    $(chart_icon).click(function(){
+        
+        $(mobile_view_stats).slideToggle(300)
+        $(chart_icon).toggleClass("active-stat-toggle")
+    })
+
+    
 })
